e24fbc8e422d9f80f630dca41206e2250fc9c78f
Valmir Filho <valmirnogueira514@gmail.com>
2025-05-06 12:00:34 -0300

    :heavy-plus-sign: build: add vitest

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,11 @@
 import { http, HttpResponse } from "msw"
 
 export const handlers = [
-    http.post("http://localhost:3000/login", () => {
+    http.post("http://localhost:3000/api/usuario/login", () => {
         return HttpResponse.json({token: "generic-token"}, {status: 200})
     }),
 
-    http.post("http://localhost:3000/signup", () => {
+    http.post("http://localhost:3000/api/usuario/cadastro", () => {
         return HttpResponse.json({token: "generic-token"}, {status: 200})
     })
 
