8c09b458bd835596aa4f253da529a1b9de7071d5
icaroo-oliveira <icarooo10.j@gmail.com>
2025-05-08 20:27:31 -0300

    feat:add novos servic

--- src/services/easydossie.service.ts
+++ src/services/easydossie.service.ts
@@ -8,7 +8,19 @@ const client = axios.create({
 });
 
 export const apiService = {
-  login: async (data: LoginData) =>
-    client.post("/api/usuario/login", data),
+  login: async (data: LoginData) => client.post("/api/usuario/login", data),
   signup: async (data: SignUpData) => client.post("/api/usuario/cadastro", data),
+  getTurmaById: async (id: number) => client.get(`/turmas/${id}`),
+  updateTurma: async (id: number, data: any) => client.put(`/turmas/${id}`, data),
+  getAlunosByIdTurma: async (data:any) => client.get(`/return_students_class`,{params:data}),
+  createAluno: async (data: any) => client.post("/add_student_manually", data),
+  deleteAluno:async (id: number, idClass: number) => client.delete("/remove_student", {params: { id: id, idClass: idClass }}),
+
+
+  editStudent: async (data: {
+    id: number;
+    name: string;
+    registration: string;
+    idClass: number;
+  }) => client.patch("/edit_student", data),
 };
