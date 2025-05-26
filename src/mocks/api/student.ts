1d41fa97232879c16d12b0e5b124456e9d17381b
Valmir Filho <valmirnogueira514@gmail.com>
2025-05-26 01:21:21 -0300

    :sparkles: feat: student mocks

--- src/mocks/api/student.ts
+++ src/mocks/api/student.ts
@@ -1,1 +1,9 @@
-export const studentMocks = []
+import { http, HttpResponse } from "msw";
+import { routeTo } from "../utils/routeTo";
+import { mocked_students } from "../utils/mockedStudents";
+
+export const studentMocks = [
+    http.get(routeTo(`/students/classes/:classId`), () => {
+        return HttpResponse.json({students: mocked_students}, {status: 200})
+    })
+]
