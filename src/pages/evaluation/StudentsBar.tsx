de341ae0227629ab9222d88b391039c394728c78
Valmir Filho <valmirnogueira514@gmail.com>
2025-06-08 16:19:12 -0300

    add react-to-print

--- src/pages/evaluation/StudentsBar.tsx
+++ src/pages/evaluation/StudentsBar.tsx
@@ -1,6 +1,7 @@
-import { Button, Divider, Grid } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, Grid } from '@mui/material';
 import StudentsCarousel from './StudentsCarousel';
 import { apiService } from "../../services/easydossie.service"
+import { useState } from 'react';
 
 interface StudentsBarProps {
     canExport: boolean;
@@ -9,16 +10,23 @@ interface StudentsBarProps {
 }
 
 const StudentsBar = ({ canExport, classId, dossierId }: StudentsBarProps) => {
+    const [openDialog, setOpenDialog] = useState(false)
+
     // Função para chamar a API de finalizar dossiê
     const handleFinalize = async () => {
         try {
-            console.log("Chamada para a API de finalização do dossiê...");
             const response = await apiService.finalizeStudentDossier(classId, dossierId);
-                        
+            setOpenDialog(true)
             alert("Dossiê finalizado com sucesso!");
         } catch (error) {
             alert("Ocorreu um erro ao finalizar o dossiê.");
         }
+    };
+
+    const handleClose = () => setOpenDialog(false);
+    const handleConfirm = () => {
+        console.log('Ação confirmada!');
+        handleClose();
     };
 
     return (
@@ -28,10 +36,10 @@ const StudentsBar = ({ canExport, classId, dossierId }: StudentsBarProps) => {
                     <StudentsCarousel />
                 </Grid>
                 <Grid paddingRight={5} size={4} display="flex" alignItems="center" justifyContent="flex-end">
-                    <Button 
-                        variant="contained" 
-                        color="success" 
-                        size='large' 
+                    <Button
+                        variant="contained"
+                        color="success"
+                        size='large'
                         disabled={!canExport}
                         onClick={handleFinalize} // Chama a função handleFinalize ao clicar
                     >
@@ -40,6 +48,26 @@ const StudentsBar = ({ canExport, classId, dossierId }: StudentsBarProps) => {
                 </Grid>
             </Grid>
             <Divider sx={{ my: 0.5 }} /> {/* Espaçamento vertical acima e abaixo */}
+
+            <Dialog
+                open={openDialog}
+                onClose={handleClose}
+                aria-labelledby="confirm-dialog-title"
+                aria-describedby="confirm-dialog-description"
+            >
+                <DialogTitle id="confirm-dialog-title">Confirmar exportação</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="confirm-dialog-description">
+                        Tem certeza que deseja finalizar e exportar os dossiês?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color='success'>Cancelar</Button>
+                    <Button onClick={handleConfirm} color="success" variant="contained" autoFocus>
+                        Confirmar
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 }
